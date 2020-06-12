0396d5ae74b2550090154ba420f678393f26f43a
Frigo <eduardohdezf@gmail.com>
2020-06-12 19:59:38 +0200

    First Commit

---
+++ SocialGames.TechnicalTest/SocialGames.TechnicalTest.Api/Controllers/GamesControllers.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SocialGames.TechnicalTest.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GamesControllers : ControllerBase
+    {
+        [Route("games/{gameId}/play")]
+        [HttpPost]
+        public async Task<IActionResult> Play(string gameId)
+        {
+            return Ok(gameId);
+        }
+    }
+}
---
+++ SocialGames.TechnicalTest/SocialGames.TechnicalTest.Api/GamesBootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using LightInject;
+
+namespace SocialGames.TechnicalTest.Api
+{
+    public class GamesBootstrapper
+    {
+        IServiceContainer _serviceContainer;
+
+        public GamesBootstrapper(IServiceContainer container)
+        {
+            _serviceContainer = container;
+        }
+
+        public void Run()
+        {
+            RegisterServices();
+        }
+
+        protected void RegisterServices()
+        {
+
+        }
+    }
+}
