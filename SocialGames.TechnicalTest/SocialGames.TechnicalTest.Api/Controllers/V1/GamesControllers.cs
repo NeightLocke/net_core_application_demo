31a5e572767e1e3edeaa2413024ad0bac86959fc
Frigo <eduardohdezf@gmail.com>
2020-06-14 16:12:33 +0200

    Improving

--- SocialGames.TechnicalTest/SocialGames.TechnicalTest.Api/Controllers/V1/GamesControllers.cs
+++ SocialGames.TechnicalTest/SocialGames.TechnicalTest.Api/Controllers/V1/GamesControllers.cs
@@ -1,5 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System.Net;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace SocialGames.TechnicalTest.Api.Controllers.V1
 {
@@ -8,12 +10,28 @@ namespace SocialGames.TechnicalTest.Api.Controllers.V1
     [ApiVersion("1")]
     public class GamesControllers : ControllerBase
     {
+        private readonly ILogger<GamesControllers> _logger;
+
+        public GamesControllers(ILogger<GamesControllers> logger)
+        {
+            _logger = logger;
+        }
+
         [Route("games/{gameId}/play")]
         [MapToApiVersion("1")]
         [HttpPost]
         public async Task<IActionResult> Play(string gameId)
         {
-            return Ok(gameId);
+            _logger.LogInformation(nameof(Play));
+            try
+            {
+                return Ok(gameId);
+            }
+            catch (System.Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return StatusCode((int)HttpStatusCode.InternalServerError);
+            }
         }
     }
 }
