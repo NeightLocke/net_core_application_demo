91cd0f4b24920828b48844e58824e314b0f6acea
Frigo <eduardohdezf@gmail.com>
2020-06-16 20:16:26 +0200

    Implementing Algorithm Game Service

---
+++ SocialGames.TechnicalTest/SocialGames.TechnicalTest.Games/DTOs/Responses/GameSingleDataResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialGames.TechnicalTest.Games.DTOs.Responses
+{
+    public class GameSingleDataResponse
+    {
+        public string Name { get; set; }
+
+        public IList<CharIndex> IndexGameDataCollection { get; set; }
+    }
+}
--- SocialGames.TechnicalTest/SocialGames.TechnicalTest.Games/Implementations/ExternalServices/GamesEvaluator.cs
+++ SocialGames.TechnicalTest/SocialGames.TechnicalTest.Games/Implementations/ExternalServices/GamesEvaluator.cs
@@ -1,27 +1,55 @@
-﻿using SocialGames.TechnicalTest.Games.DTOs;
+﻿using Microsoft.Extensions.Logging;
+using SocialGames.TechnicalTest.Games.DTOs;
 using SocialGames.TechnicalTest.Games.DTOs.Request;
 using SocialGames.TechnicalTest.Games.DTOs.Responses;
 using SocialGames.TechnicalTest.Games.Interface.ExternalServices;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace SocialGames.TechnicalTest.Games.Implementations.ExternalServices
 {
     public class GamesEvaluator : IGamesEvaluator
     {
-        public GamesResponse EvaluateGames(GamesRequest request)
+        private const char BreakingCharacter = 'o';
+        private readonly ILogger<GamesEvaluator> _logger;
+
+        public GamesEvaluator(ILogger<GamesEvaluator> logger)
         {
-            var charIndexItem = new CharIndex() { Char = 'A', Index = 0 };
-            var charIndexCollection = new List<CharIndex>() { charIndexItem };
-            var response = new GamesResponse()
+            _logger = logger;
+        }
+
+        public async Task<GamesResponse> EvaluateGames(GamesRequest request)
+        {
+            var response = new GamesResponse();
+            response.GamesDataCollection = new List<GameSingleDataResponse>();
+            request.Games.ToList().ForEach(game =>
             {
-                GamesDataCollection = charIndexCollection
-            };
-            Thread.Sleep(5000);
+                response.GamesDataCollection.Add(GetGameSingleDataResponse(game));
+            });
+            await Task.Delay(500);
             return response;
+        }
+
+        private GameSingleDataResponse GetGameSingleDataResponse(string game)
+        {
+            var result = new GameSingleDataResponse();
+            result.Name = game;
+            result.IndexGameDataCollection = new List<CharIndex>();
+            for (int i = 0; i < game.Length; i++)
+            {
+                if (game[i].Equals(BreakingCharacter))
+                {
+                    break;
+                }
+                var charIndex = new CharIndex() { Char = game[i], Index = i };
+                result.IndexGameDataCollection.Add(charIndex);
+            }
+            return result;
         }
     }
 }
